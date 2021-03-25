3ff1a5c36c89d349fc3085c5dafe67a105c2d005
ilias belkhder <23727@student.dorset-college.ie>
2021-03-25 20:01:25 +0000

    Get request done

--- Models/Book_description.cs
+++ Models/Book_description.cs
@@ -9,5 +9,28 @@ namespace deploy_test.Models
         public int book_id { get; set; }
         public string book_name { get; set; }
         public string book_description { get; set; }
+		
+		[HttpGet("{id}")]
+        public ActionResult<BookDTO> GetBooks_byId(int id)
+        {
+            var book = from books in _context.Book
+            join book_descriptions in _context.Book_Description on books.id equals book_descriptions.book_id
+            select new BookDTO
+            {
+                Book_id = books.id,
+                Book_price = books.price,
+                ISBN = books.isbn,
+                Book_name = book_descriptions.book_name,
+                Book_description = book_descriptions.book_description
+            };
+
+            var book_by_id = book.ToList().Find(x => x.Book_id == id);
+
+            if (book_by_id == null)
+            {
+                return NotFound();
+            }
+            return book_by_id;
+        }
     }
 }
