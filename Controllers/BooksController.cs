8fc31963266b4b39dff8d20864a6bc5fcc215141
Guilhem <guilhem.carlet@epita.fr>
2021-03-25 19:38:20 +0000

    Added Data folder and BookController

---
+++ Controllers/BooksController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace deploy_test.Controllers
+{
+    public class BooksController
+    {
+    }
+}
