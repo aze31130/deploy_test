ec94cd742d8e193e18bbc84e4b282343907b7aaf
Guilhem <guilhem.carlet@epita.fr>
2021-03-25 19:35:56 +0000

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using deploy_test.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace deploy_test.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
