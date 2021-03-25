44ba60a83bf832b1cbcd2f02ea70f180f529ea91
Guilhem <guilhem.carlet@epita.fr>
2021-03-25 19:48:58 +0000

    Added Model classes and Context file

---
+++ Data/Context.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace deplot_test.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+        public DbSet<Book> Book { get; set; }
+        public DbSet<Book_description> Book_Description { get; set; }
+    }
+}
