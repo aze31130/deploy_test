b5459535c63dd16aa133613f8561ad433273c2ec
Guilhem <guilhem.carlet@epita.fr>
2021-03-25 20:50:50 +0000

    Added Addbook class model

---
+++ Models/AddBook.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace backend_database_HTTP_Requests.DTO
+{
+    public class AddBook
+    {
+        internal IEnumerable<object> books;
+
+        [Required]
+        public decimal Book_price { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Book_name { get; set; }
+        [Required]
+        public string Book_description { get; set; }
+    }
+}
