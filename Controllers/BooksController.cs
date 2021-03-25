7bb4e7aad6e1b90400780c02dfec06b0a8e52bed
Marin BAILHE <marin.bailhe@epita.fr>
2021-03-25 19:56:19 +0000

    added first part of the bookcontroller

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -1,11 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using deploy_test.Data;
+using deploy_test.DTO;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using deploy_test.Models;
+using DTO;
+
 
 namespace deploy_test.Controllers
 {
-    public class BooksController
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
     {
+        private readonly Context _context;
+
+        public BooksController(Context context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookDTO>>> GetBooks()
+        {
+            var book = from books in _context.Book
+                       join book_descriptions in _context.Book_Description on books.id equals book_descriptions.book_id
+                       select new BookDTO
+                       {
+                           Book_id = books.id,
+                           Book_price = books.price,
+                           ISBN = books.isbn,
+                           Book_name = book_descriptions.book_name,
+                           Book_description = book_descriptions.book_description
+                       };
+
+            return await book.ToListAsync();
+        }
     }
 }
